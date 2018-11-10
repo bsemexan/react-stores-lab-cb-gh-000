5b85f75b7ec992f617d6489ac00a748b17764d97
Berry <bsemexan@gmail.com>
2018-11-09 20:44:56 -0500

    Done.

--- stores/Store.js
+++ stores/Store.js
@@ -1,7 +1,34 @@
+import { removeListener } from "cluster";
+
 class Store {
   // Your implementation here.
   // Hint: Our <App /> component won't directly use this store, but instead the
   // CounterStore is going to inherit from it.
+  constructor (initialState) {
+    this.state = initialState;
+    this.listeners = [];
+  }
+ 
+  setState (state) {
+    this.state = state;
+    for (const listener of this.listeners) {
+      listener(state);
+    }
+  }
+ 
+  getState () {
+    return this.state;
+  }
+
+  addListener(listener) {
+    this.listeners.push(listener);
+    
+    const removeListener = () => {
+      this.listeners = this.listeners
+        .filter(l => listener !== l);
+    };
+    return removeListener;
+  }
 }
 
 export default Store;
